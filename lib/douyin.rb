c4a1220ee8a0db3ac74b0df422656fbf6ac59952
anthony <zengkun@netfarmer.com.cn>
2023-08-29 15:01:52 +0800

    fix:

--- lib/douyin.rb
+++ lib/douyin.rb
@@ -6,8 +6,11 @@ require 'active_support/all'
 require 'douyin/config'
 require 'douyin/exceptions'
 douyin_lib_path = "#{File.dirname(__FILE__)}/douyin"
-Dir["#{douyin_lib_path}/api/methods/*.rb", "#{douyin_lib_path}/token_store/*.rb"].each { |path| require path }
+Dir["#{douyin_lib_path}/api/methods/*.rb"].each { |path| require path }
 
+require 'douyin/token_store/base'
+require 'douyin/token_store/app_token'
+require 'douyin/token_store/user_token'
 require 'douyin/api/base'
 require 'douyin/api/app'
 require 'douyin/api/user'
